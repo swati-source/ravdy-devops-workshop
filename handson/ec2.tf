74a0189f5c96e2321a3ee8f5a92336f84915bf8c
Satigoudar <swati-shivanand.satigoudar@capgemini.com>
2024-06-20 22:42:15 +0530

    created ec2 with security grp

---
+++ handson/ec2.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_security_group" "demo_sg" {
+  name        = "demo_sg"
+  description = "SSH Access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "demo-server" {
+  ami           = "ami-08a0d1e16fc3f61ea"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"
+  key_name = "Terraform"
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
+
+  tags = {
+    Name = "demo-server"
+  }
+}
+
+
